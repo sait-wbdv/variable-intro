bed7cb626d99e9108069fb1a7ef864e92799686c
Ashlyn Knox <ashlyn@lilyx.io>
2024-07-25 12:51:14 -0600

    activity file

---
+++ index.js
@@ -0,0 +1,34 @@
+/* -- Part 1 -- */
+
+// Fix the following variables
+
+CONST MYNAME = "Sarah";
+
+let favorite-food = "ice-cream";
+
+let ishungry = "yes";
+
+/* -- Part 2 -- */
+
+// name variable: string
+
+// favourite number variable: number
+
+// preference for pizza variable: boolean
+
+// log each of the variables to the console in a single output.
+
+
+/* -- Part 3 -- */
+
+// change the vars to const or let depending on if the variable would change or not
+
+var playerScore = 23;
+
+var age = 30;
+
+var name = "Bobby Bonanza";
+
+var isAwake = false;
+
+var originCountry = "Canada";
